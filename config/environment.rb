1ebc383aa541e9f698935f23bc8f2b09cc3e3926
yass8n <yass8n@yahoo.com>
2015-03-14 23:33:00 -0700

    testing send grid

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,13 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.delivery_method = :ses
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'yaniss',
+  :password => 'Password3',
+  :domain => 'ec2-54-69-64-152.us-west-2.compute.amazonaws.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
