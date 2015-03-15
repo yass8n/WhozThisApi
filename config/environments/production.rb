68e808029f3cbbf1d650b65368b933996207b2a7
yass8n <yass8n@yahoo.com>
2015-03-14 23:34:19 -0700

    testing emails with send grid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,15 @@ Rails.application.configure do
   config.action_controller.include_all_helpers = true
 
   config.assets.initialize_on_precompile = true
+
+  ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'yaniss',
+  :password => 'Password3',
+  :domain => 'ec2-54-69-64-152.us-west-2.compute.amazonaws.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
 end
