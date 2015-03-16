00273829f4242ce04fb7b648af0af75577b057cf
yass8n <yass8n@yahoo.com>
2015-03-16 07:08:44 -0700

    testing

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -70,7 +70,6 @@ class API::V1::UsersController < ApplicationController
     # look at api/v1/users/freinds.json.jbuilder for output
     #passing an array of phones to the database to match the users phones and return friends
     @friends = User.where(phone: params[:phones])
-    render json: @friends and return
   end
 
 
