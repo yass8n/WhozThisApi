3c4be4a5f985bce44773538b292598b1fc32a675
yass8n <yass8n@yahoo.com>
2015-03-21 21:32:21 -0700

    can now upload photo on Sign Up

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -29,9 +29,10 @@ class API::V1::UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    if !params[:user].nil? && !params[:user][:filename].nil?
-      @user.create_image
+    encoded_file = params[:user][:filename]
+    if !params[:user].nil? && !encoded_file.nil? && encoded_file != ""
+      @user.remove_image_path
+      @user.create_image(encoded_file)
     end
 
     respond_to do |format|
